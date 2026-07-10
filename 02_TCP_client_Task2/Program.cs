9574982167baeb39a3f459dcef30ddfb7177ab1e
AndrewHale01 <galevskiy2009@gmail.com>
2026-07-10 09:13:33 +0300

    done task2 socket_server_client

---
+++ 02_TCP_client_Task2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace SoketClient
+{
+    internal class Program
+    {
+        static int port = 8080;
+        static string address = "127.0.0.1";
+        static void Main(string[] args)
+        {
+            try
+            {
+                IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse(address), port);
+                Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+                socket.Connect(ipPoint);
+
+                string message = "";
+                while (message != "end")
+                {
+                    Console.WriteLine("Do you want show date or time");
+                    message = Console.ReadLine()!;
+                    byte[] data = Encoding.Unicode.GetBytes(message);
+                    socket.Send(data);
+
+                    int bytes = 0;
+                    string response = "";
+                    data = new byte[1024];
+                    do
+                    {
+                        bytes = socket.Receive(data);
+                        response = Encoding.Unicode.GetString(data, 0, bytes);
+                        Console.WriteLine($"Server response : {response}");
+                    } while (socket.Available > 0);
+
+                }
+
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+
+        }
+    }
+}
---
+++ 02_TCP_server_Task2/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace SoketServer
+{
+    internal class Program
+    {
+        static int port = 8080;
+        static string address = "127.0.0.1";
+        static void Main(string[] args)
+        {
+            IPEndPoint iPEndPoint = new IPEndPoint(IPAddress.Parse(address), port);
+
+
+            Socket listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                listenSocket.Bind(iPEndPoint);
+                Console.WriteLine("Server stated! Waiting for connection...");
+                listenSocket.Listen(10);
+                Socket client = listenSocket.Accept();
+                string response_client = "";
+                Console.WriteLine("Client connected");
+                while (true)
+                {
+                    int bytes = 0;
+                    byte[] data = new byte[1024];
+                    bytes = client.Receive(data);
+
+                    string msg = Encoding.Unicode.GetString(data, 0, bytes);
+                    Console.WriteLine($"{DateTime.Now.ToShortTimeString()} : {msg}" + " " +
+                        $"from {client.RemoteEndPoint}");
+                    if (msg == "date")
+                    {
+                        response_client = $"Current date is {DateTime.Now.ToShortDateString()}";
+                    }
+                    else if (msg == "time")
+                    {
+                        response_client = $"Current time is {DateTime.Now.ToShortTimeString()}";
+                    }
+                    else if (msg != "time" || msg != "date")
+                    {
+                        response_client = $"False message";
+                    }
+                    else if (msg == "end")
+                    {
+                        response_client = "Goodbye!";
+                    }
+
+                    data = Encoding.Unicode.GetBytes(response_client);
+                    client.Send(data);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            listenSocket.Shutdown(SocketShutdown.Both);
+            listenSocket.Close();
+        }
+    }
+}
